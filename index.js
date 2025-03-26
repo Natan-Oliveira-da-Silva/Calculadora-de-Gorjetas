af9e9bf687cfd65d58da3c728a04ee618e6e500a
Natan Oliveira da Silva <natan_o_da_silva@outlook.com>
2025-03-26 16:19:36 -0200

    Adição dos arquivos HTML, CSS e JavaScript

---
+++ index.js
@@ -0,0 +1,20 @@
+function formatMoney(valor){
+    valor = valor.toFixed(2)
+    return "R$ " + valor
+}
+
+function update(){
+    let bill = Number(document.getElementById("yourBill").value)
+    let tipPercent = document.getElementById("tipInput").value
+    let split = document.getElementById("splitInput").value
+
+    let tipValue = bill * (tipPercent / 100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+
+    document.getElementById("tipPercent").innerHTML = tipPercent+"%"
+    document.getElementById("tipValue").innerHTML = formatMoney(tipValue)
+    document.getElementById("totalWithTip").innerHTML = formatMoney(billTotal)
+    document.getElementById("splitValue").innerHTML = split == 1 ? "1 pessoa" : split+" pessoas"
+    document.getElementById("billEach").innerHTML = formatMoney(billEach)
+}
